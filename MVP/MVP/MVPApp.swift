f68ec07556cc4a4beb31fab5278c952166520cfa
万力铭 <wanliming@douyu.tv>
2023-03-05 23:41:55 +0800

    init proj

---
+++ MVP/MVP/MVPApp.swift
@@ -0,0 +1,18 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Murloc Wan on 2023/3/5.
+//
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
